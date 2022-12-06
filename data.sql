ba6d158a80f28b81dfad190ff763362c6d0418cb
Derrick <djmulinde@gmail.com>
2022-12-06 16:56:08 +0300

    Add insert data statements

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Agumon', '03/02/2020', 0, TRUE, 10.23);
+insert into animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Gabumon', '07/01/2021', 2, TRUE, 8.00);
+insert into animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Pikachu', '07/01/2021', 1, FALSE, 15.04);
+insert into animals(name, date_of_birth, escape_attempts, neutered, weight_kg) values('Devimon', '12/05/2017', 5, TRUE, 11.00);
