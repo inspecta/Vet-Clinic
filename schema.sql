155ad87c5d80a61be69867f74be7a9948ef3c537
Derrick <djmulinde@gmail.com>
2022-12-07 22:38:33 +0300

    Add SQL constraints statements

--- schema.sql
+++ schema.sql
@@ -10,3 +10,27 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(20);
+
+/* Create owners table */
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT
+);
+
+/*Create Species table */
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+/*Constraints */
+ALTER TABLE animals 
+ADD COLUMN species_id INT CONSTRAINT species_animals_fk 
+REFERENCES species(id)
+ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INT CONSTRAINT owners_animals_fk 
+REFERENCES owners(id) 
+ON UPDATE CASCADE ON DELETE CASCADE;
