e42bb63221a37ccee731f26d5e5bd9779dda503f
Derrick <djmulinde@gmail.com>
2022-12-12 18:33:19 +0300

    Add insert sql statements

--- data.sql
+++ data.sql
@@ -212,3 +212,12 @@ VALUES(
 (SELECT id FROM animals WHERE name = 'Blossom'),
 (SELECT id FROM vets WHERE name = 'William Tatcher'),
 '11/01/2021');
+
+-- Performance Tuning
+INSERT INTO visits (animal_id, vet_id, visit_date)
+SELECT * FROM (SELECT id FROM animals) animal_ids,
+(SELECT id FROM vets)
+vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+INSERT INTO owners (full_name, email)
+SELECT 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
