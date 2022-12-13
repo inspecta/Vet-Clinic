70e7f3dae56cea81d998560e904561723a07b6bc
Daron Wiafe <daronkwiafe@gmail.com>
2022-12-13 15:24:44 +0200

    complete database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,3 +24,27 @@ CREATE TABLE invoices(
     CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
 
+CREATE TABLE treatments(
+id INTEGER NOT NULL,
+type VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories_treatments(
+medical_histories_id INTEGER NOT NULL,
+treatment_id INTEGER NOT NULL,
+PRIMARY KEY(medical_histories_id, treatment_id)
+);
+
+CREATE TABLE invoice_items(
+id INTEGER NOT NULL,
+unit_price DECIMAL(8, 2) NOT NULL,
+quantity INTEGER NOT NULL,
+total_price DECIMAL(8, 2) NOT NULL,
+invoice_id INTEGER NOT NULL,
+treatment_id INTEGER NOT NULL,
+PRIMARY KEY(id),
+CONSTRAINT invoice_items_invoice_id_foreign FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+); 
